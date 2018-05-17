f2405bdd14a87428892d81f1e0ed23e88f35b2bf
David Letts <dgletts@gmail.com>
2018-05-16 22:55:44 -0400

    Initial commit

---
+++ tweets.py
@@ -0,0 +1,20 @@
+import tweepy
+from textblob import TextBlob
+
+consumer_key = "MWPfhlK89zKnVZvtISuSvTlyd"
+consumer_secret = "mu0csop48M2tKbND2Cvu4HlslncVtbB09jJY5rx8sbYAEwfgeq"
+
+access_token = "441556445-BX9Ij1hETlvU53Dx1f2eiiCr29RFTMJfTMK6RVWO"
+access_token_secret = "o2rEjnPXKSAd2bg7JyzB9O4HFBEka2nZ5h7RzlBcL8W9N"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('fortnite')
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+	print(analysis.sentiment)
